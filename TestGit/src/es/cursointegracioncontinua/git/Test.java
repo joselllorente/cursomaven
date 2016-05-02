7d04460353b0460efa4fc2531b6a3fdf0216ebf7
Jose Luis Llorente <joseluis.llorenteperales@gmail.com>
2016-05-02 11:52:31 +0200

    NuevoTest

--- TestGit/src/es/cursointegracioncontinua/git/Test.java
+++ TestGit/src/es/cursointegracioncontinua/git/Test.java
@@ -3,7 +3,7 @@ package es.cursointegracioncontinua.git;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Probando");
+		System.out.println("Probando2");
 
 	}
 
